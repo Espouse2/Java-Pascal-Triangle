d1afb57b3ebe3d7d3f83d2305f459505891eb1cd
David Rędzia <64521895+Espouse2@users.noreply.github.com>
2020-04-29 14:01:32 +0200

    Updated model - update 2.1
    - Change binomial coeffcieint calculating method: there was a lack of paranthesis which resulted in wrong calculations.
    
    - Updated names of lists to fit better into the programme. It will be more understandable.

--- model/Model.java
+++ model/Model.java
@@ -36,7 +36,7 @@ public class Model {
      */
 
     public int binomialCoefficient(int n, int r) {
-        return factorial(n) / factorial(r) * factorial(n - r);
+        return factorial(n) /( factorial(r) * factorial(n - r));
     }
 
     /**
@@ -59,35 +59,22 @@ public class Model {
     }*/
 
     public List<List<Integer>> pascalTriangleBinCoeff2(int numberOfRows){
-        /*
         List<List<Integer>> array = new ArrayList<List<Integer>>(numberOfRows);
 
-        for(int i = 0; i < numberOfRows; i++){
-            array.add(new ArrayList<Integer>());
-            for(int j = 0; j <= i; j++){
-                array.get(i).add(binomialCoefficient(i, j));
-            }
-        }
-        return array;*/
-        List<List<Integer>> array = new ArrayList<List<Integer>>(numberOfRows);
-
-        if(numberOfRows <= 0) return array;
-
-        ArrayList<Integer> first = new ArrayList<Integer>();
-        first.add(1);
-        array.add(first);
+        ArrayList<Integer> firstElement = new ArrayList<Integer>();
+        firstElement.add(1);
+        array.add(firstElement);
 
         for (int i=1; i < numberOfRows; i++){
 
-            List<Integer> before = array.get(i-1);
-            ArrayList<Integer> now = new ArrayList<Integer>();
-            now.add(1);
+            ArrayList<Integer> currentRow = new ArrayList<Integer>();
+            currentRow.add(1);
 
             for(int j=1; j < i; j++){
-                now.add(binomialCoefficient(i, j));
+                currentRow.add(binomialCoefficient(i, j));
             }
-            now.add(1);
-            array.add(now);
+            currentRow.add(1);
+            array.add(currentRow);
         }
         return array;
     }
@@ -97,45 +84,25 @@ public class Model {
      * @param numberOfRows - it gets number of rows user wants to display
      * @return array of values to be displayed using view class
      */
-    /*
-    public int[][] pascalTriangleSum(int numberOfRows){
-        int[][] array;
-        array = new int[numberOfRows][numberOfRows];
-
-        for(int i = 0; i < numberOfRows; i++){
-
-            for(int j = 0; j <= i; j++){
-                if(i == j || j == 0){
-                    array[i][j] = 1;
-                }
-                else{
-                    array[i][j] = array[i-1][j-1] + array[i-1][j];
-                }
-            }
-        }
-        return array;
-    }*/
-
+  
     public List<List<Integer>> pascalTriangleSum2(int numberOfRows){
         List<List<Integer>> array = new ArrayList<List<Integer>>(numberOfRows);
 
-        if(numberOfRows <= 0) return array;
-
-        ArrayList<Integer> first = new ArrayList<Integer>();
-        first.add(1);
-        array.add(first);
+        ArrayList<Integer> firstElement = new ArrayList<Integer>();
+        firstElement.add(1);
+        array.add(firstElement);
 
         for (int i=1; i < numberOfRows; i++){
 
-            List<Integer> before = array.get(i-1);
-            ArrayList<Integer> now = new ArrayList<Integer>();
-            now.add(1);
+            List<Integer> previousRow = array.get(i-1);
+            ArrayList<Integer> currentRow = new ArrayList<Integer>();
+            currentRow.add(1);
 
             for(int j=1; j < i; j++){
-               now.add(before.get(j-1) + before.get(j));
+               currentRow.add(previousRow.get(j-1) + previousRow.get(j));
             }
-            now.add(1);
-            array.add(now);
+            currentRow.add(1);
+            array.add(currentRow);
         }
         return array;
     }
